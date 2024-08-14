8af21eb475e9b89eb45d12af53f1dca1459f20be
TPK Healy <thomaspkhealy@yahoo.com>
2024-08-14 18:21:02 +0100

    OCX support added to wxModularCore

--- wxModularCore/wxModularCore.h
+++ wxModularCore/wxModularCore.h
@@ -53,8 +53,7 @@ protected:
 			if (!dll) // Probably plugin was not loaded from dll
 				break;
 
-			wxDYNLIB_FUNCTION(DeletePluginFunctionType, 
-				DeletePlugin, *dll);
+			auto const pfnDeletePlugin = (DeletePluginFunctionType) dll->RawGetSymbol(wxT("DeletePlugin"));
 			if (pfnDeletePlugin)
 			{
 				pfnDeletePlugin(plugin);
@@ -102,39 +101,130 @@ protected:
 		const wxString & subFolder)
 	{
 		wxFileName fn;
-		fn.AssignDir(pluginsDirectory);
-		wxLogDebug(wxT("%s"), fn.GetFullPath().data());
-		fn.AppendDir(subFolder);
-		wxLogDebug(wxT("%s"), fn.GetFullPath().data());
-		if (!fn.DirExists())
-			return false;
+		fn.AssignDir(pluginsDirectory);	   wxLogDebug(wxT("%s"), fn.GetFullPath().data());
+		fn.AppendDir(subFolder);           wxLogDebug(wxT("%s"), fn.GetFullPath().data());
+		if ( !fn.DirExists() ) return false;
 
 		if(!wxDirExists(fn.GetFullPath())) return false;
-		wxString wildcard = wxString::Format(wxT("*.%s"), 
-			GetPluginExt().GetData());
+		wxString wildcard = wxString::Format(wxT("*.%s"), GetPluginExt().GetData());
 		wxArrayString pluginPaths;
-		wxDir::GetAllFiles(fn.GetFullPath(), 
-			&pluginPaths, wildcard);
-		for(size_t i = 0; i < pluginPaths.GetCount(); ++i)
+		wxDir::GetAllFiles(fn.GetFullPath(), &pluginPaths, wildcard);
+		wxDir::GetAllFiles(fn.GetFullPath(), &pluginPaths, "*.ocx");
+
+		for ( size_t i = 0; i < pluginPaths.GetCount(); ++i )
 		{
 			wxString fileName = pluginPaths[i];
-			wxDynamicLibrary * dll = new wxDynamicLibrary(fileName);
-			if (dll->IsLoaded())
+			auto *dll = new wxDynamicLibrary(fileName);
+			if ( false == dll->IsLoaded() ) continue;
+
+			auto const pfnCreatePlugin = (CreatePluginFunctionType)dll->RawGetSymbol(wxT("CreatePlugin"));
+
+			if (pfnCreatePlugin)
 			{
-				wxDYNLIB_FUNCTION(CreatePluginFunctionType, 
-					CreatePlugin, *dll);
-				if (pfnCreatePlugin)
-				{
-					PluginType * plugin = pfnCreatePlugin();
-					RegisterPlugin(plugin, list);
-					m_DllList.Append(dll);
-					pluginDictionary[plugin] = dll;
-				}
-				else
-					wxDELETE(dll);
+				PluginType* plugin = pfnCreatePlugin();
+				RegisterPlugin(plugin, list);
+				m_DllList.Append(dll);
+				pluginDictionary[plugin] = dll;
+				continue;
 			}
+
+			PluginType *const plugin = Process_ActiveX_Plugin<PluginType>(dll);
+			if ( plugin )
+			{
+				RegisterPlugin(plugin, list);
+				m_DllList.Append(dll);
+				pluginDictionary[plugin] = dll;
+			}
+			else wxDELETE(dll);
 		}
+
 		return true;
 	}
 
 };
+
+#include <wx/dynlib.h>    // wxDynamicLibrary
+
+#ifndef __WXMSW__
+
+template<class PluginType>
+PluginType *Process_ActiveX_Plugin(wxDynamicLibrary*)
+{
+	// At the moment, only wxMSW can load an OCX plugin,
+	// but maybe in the future, the 'Wine' emulator running
+	// on Linux will be able to load an ActiveX control.
+	return false;
+}
+
+#else
+
+#include <cassert>        // assert
+#include <new>            // new(nothrow)
+#include <type_traits>    // is_abstract_v
+#include <wx/string.h>    // wxString
+#include <wx/window.h>    // wxWindow
+
+extern "C" {
+	void OCX_Release_IOleObject(void *p_IOleObject);                  // defined in wxModularCoreOCX.cpp
+	wxWindow *OCX_CreatePanel(void *p_IOleObject, wxWindow *parent);  // defined in wxModularCoreOCX.cpp
+	void *OCX_Process_ActiveX_Plugin(wxDynamicLibrary *dll);          // defined in wxModularCoreOCX.cpp
+}
+
+template<class PluginType>
+class wxGuiPluginOCX : public PluginType {
+protected:
+	void *const p_IOleObject;
+
+public:
+	using PluginType::GetName;
+	using PluginType::GetId;
+	using PluginType::CreatePanel;
+
+	virtual wxString GetName(void) const override { return "Name OCX Plugin 0"; }
+	virtual wxString GetId  (void) const override { return "ID   OCX Plugin 0"; }
+
+	virtual wxWindow *CreatePanel(wxWindow *const parent) override
+	{
+		assert( nullptr != this->p_IOleObject );
+		assert( nullptr != parent             );
+		return OCX_CreatePanel(this->p_IOleObject, parent);
+	}
+
+	wxGuiPluginOCX(void *const arg_p_IOleObject) : PluginType( nullptr /* event handler */ ), p_IOleObject(arg_p_IOleObject)
+	{
+		assert( nullptr != arg_p_IOleObject );
+	}
+
+	~wxGuiPluginOCX(void) override
+	{
+		assert( nullptr != this->p_IOleObject );
+		OCX_Release_IOleObject(this->p_IOleObject);
+	}
+};
+
+template<class PluginType>
+PluginType *Process_ActiveX_Plugin(wxDynamicLibrary *const dll)
+{
+	if constexpr ( requires { std::declval<PluginType&>().Work(); } )
+	{
+		// In case 'PluginType' is non-GUI or some other kind of plugin
+		return nullptr;
+	}
+	else
+	{
+		void *const pole = OCX_Process_ActiveX_Plugin(dll);
+
+		if ( nullptr == pole ) return nullptr;
+
+		auto *const plugin = new(std::nothrow) wxGuiPluginOCX<PluginType>(pole);
+		if ( nullptr == plugin )
+		{
+			OCX_Release_IOleObject(pole);
+			return nullptr;
+		}
+
+		return plugin;
+	}
+}
+
+#endif
